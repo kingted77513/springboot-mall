68247decaad3443b5d80e0a8b828d257f937441f
dev41 <kingted77513@gmail.com>
2022-07-14 07:50:36 +0800

    商品功能 - 實作查詢所有商品列表 (排序)

--- src/main/java/com/teddy/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/teddy/springbootmall/dto/ProductQueryParams.java
@@ -22,4 +22,23 @@ public class ProductQueryParams {
     }
 
     private String search;
+
+    private String orderBy;
+    private String sort;
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
 }
