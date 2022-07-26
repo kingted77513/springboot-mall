d5514b0252ca3749bb9baa97fa75e0fef3d05647
dev41 <kingted77513@gmail.com>
2022-07-26 09:46:58 +0800

    訂功能 - 創建訂單 (上)

--- src/main/java/com/teddy/springbootmall/dao/OrderDao.java
+++ src/main/java/com/teddy/springbootmall/dao/OrderDao.java
@@ -1,5 +1,10 @@
 package com.teddy.springbootmall.dao;
 
+import com.teddy.springbootmall.model.OrderItem;
+import java.util.List;
+
 public interface OrderDao {
     Integer createOrder(Integer userId, int totalAmount);
+
+    void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
 }
--- src/main/java/com/teddy/springbootmall/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/teddy/springbootmall/dao/impl/OrderDaoImpl.java
@@ -1,8 +1,16 @@
 package com.teddy.springbootmall.dao.impl;
 
 import com.teddy.springbootmall.dao.OrderDao;
+import com.teddy.springbootmall.model.OrderItem;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +20,43 @@ public class OrderDaoImpl implements OrderDao {
 
     @Override
     public Integer createOrder(Integer userId, int totalAmount) {
-        return null;
+        String sql = "INSERT INTO `order`(user_id, total_amount, created_date, last_modified_date)"+
+            "VALUES (:userIid, :totalAmount, :createdDate, :lastModifiedDate)";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userIid", userId);
+        map.put("totalAmount", totalAmount);
+
+        Date now = new Date();
+        map.put("createdDate", now);
+        map.put("lastModifiedDate", now);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+
+        int orderId  = keyHolder.getKey().intValue();
+
+        return orderId;
+    }
+
+    @Override
+    public void createOrderItems(Integer orderId, List<OrderItem> orderItemList) {
+        String sql = "INSERT INTO order_item(order_id, product_id, quantity, amount) " +
+            "VALUES (:orderId, :productId, :quantity, :amount)";
+
+        MapSqlParameterSource[] parameterSources = new MapSqlParameterSource[orderItemList.size()];
+
+        for (int i = 0 ; i < orderItemList.size() ; i++){
+            OrderItem orderItem = orderItemList.get(i);
+
+            parameterSources[i] = new MapSqlParameterSource();
+            parameterSources[i].addValue("orderId", orderId);
+            parameterSources[i].addValue("productId", orderItem.getProductId());
+            parameterSources[i].addValue("quantity", orderItem.getQuantity());
+            parameterSources[i].addValue("amount", orderItem.getAmount());
+        }
+
+        namedParameterJdbcTemplate.batchUpdate(sql, parameterSources);
     }
 }
